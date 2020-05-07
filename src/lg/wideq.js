931e004c6b435da784049333e5b930f9a29c2289
Jake Mauer <jake@jakemauer.com>
2020-05-06 21:12:43 -0700

    Trying again

--- src/lg/wideq.js
+++ src/lg/wideq.js
@@ -119,8 +119,6 @@ module.exports.Wideq = function (country, language) {
         return {
             onOff: monitorLines[0],
             mode: monitorLines[1],
-            currentTemp: monitorLines[2].substring(4).replace(' C', ''),
-            targetTemp: monitorLines[3].substring(4).replace(' C', ''),
             speed: monitorLines[4].substring(10).replace('\n', '')
         };
     };
